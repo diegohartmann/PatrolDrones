475db6c4b6661d3ae9c6deecf747fa5ca882609d
Diego Hartmann <contato.hartmann8@gmail.com>
2021-05-06 13:02:14 -0300

    funcional _> movimentacao do player refatorada

--- game/IA_Drone_Proj/Assets/Drone_Player/PlayerScripts/exempleMove.cs
+++ game/IA_Drone_Proj/Assets/Drone_Player/PlayerScripts/exempleMove.cs
@@ -3,13 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public enum MoveMode{
-    rigidbody,
-    transform
-}
 public class exempleMove : MonoBehaviour
 {
-    [SerializeField]private MoveMode moveMode = MoveMode.transform;
+    [SerializeField]private bool rbMove = true;
     [SerializeField]private Transform rotatableObj = null;
     [SerializeField]private float rotationFactor = 3f;
     [SerializeField]private int groundLayer = 11;
@@ -19,32 +15,17 @@ public class exempleMove : MonoBehaviour
     [HideInInspector]public bool isOnTile;
     private Camera mainCamera;
     private void Awake() {
+        SetStuff();
+    }
+    void SetStuff(){
         mainCamera = FindObjectOfType<Camera>();
         thisRB = GetComponent<Rigidbody>();
     }
-    void Update()
-    {
-        Vector3 dir = new Vector3(Input.GetAxis("Horizontal"), 0 , Input.GetAxis("Vertical")).normalized;
-
-        switch (moveMode)
-        {
-            case MoveMode.rigidbody:
-                MoveRBTo(dir);
-                TurnToMouse(true);
-            break;
-
-            case MoveMode.transform:
-                MoveTo(dir);
-                TurnToMouse(false);
-            break;
-
-            default:
-                Debug.LogWarning("Selecione um modo de movimentacao");
-            break;
-        }
+    void Update(){  
+        Move();
+        TurnToMouse();
         CheckIfIsOnTile();
     }
-
     void CheckIfIsOnTile(){
         RaycastHit hit;
         bool rayCollided = Physics.Raycast(transform.position, Vector3.down, out hit, Mathf.Infinity);
@@ -54,37 +35,43 @@ public class exempleMove : MonoBehaviour
         }
         this.isOnTile = false;
     }
-    void TurnToMouse(bool _rb){
+    void TurnToMouse(){
         Ray cameraRay = mainCamera.ScreenPointToRay(Input.mousePosition);
         Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
         float rayLenght;
         if(groundPlane.Raycast(cameraRay, out rayLenght)){
             Vector3 pointToLook = (cameraRay.GetPoint(rayLenght));
             Vector3 finalPoint = new Vector3 (pointToLook.x,  rotateOffset  , pointToLook.z);
-            RotateTo(finalPoint, _rb);
+            RotateTo(finalPoint);
         }
     }
-    void MoveTo(Vector3 dir){
+    void Move(){
+        Vector3 dir = new Vector3(Input.GetAxis("Horizontal"), 0 , Input.GetAxis("Vertical")).normalized;
+        if(rbMove){
+            MoveRB(dir);
+            return;
+        }
+        MoveTransform(dir);
+    }
+    void MoveTransform(Vector3 dir){
         transform.Translate(dir * DeltaTime(moveSpeed));
     }
-    void MoveRBTo(Vector3 dir){
+    void MoveRB(Vector3 dir){
         dir *= DeltaTime(moveSpeed);
         thisRB.velocity = Vector3.zero;
         thisRB.angularVelocity = Vector3.zero;   
         thisRB.Sleep();
         thisRB.MovePosition(transform.position + dir);
     }
-    void RotateTo(Vector3 _target, bool _rb){
+    void RotateTo(Vector3 _target){
         var neededRotation = Quaternion.LookRotation(_target - rotatableObj.position);
         var interpolatedRotation = Quaternion.Slerp(rotatableObj.rotation, neededRotation, Time.deltaTime * rotationFactor);
-        if(_rb){
+        if(rbMove){
             thisRB.MoveRotation(interpolatedRotation);
             return;
         }
         rotatableObj.rotation = interpolatedRotation;
     }
-
-
     float DeltaTime(float _times){
         return _times * Time.deltaTime;
     }
