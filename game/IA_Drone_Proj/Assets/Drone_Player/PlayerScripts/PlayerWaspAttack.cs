d5af35a8c09b9d886ea0f127aad999aeab155302
Diego Hartmann <contato.hartmann8@gmail.com>
2021-06-17 15:37:27 -0300

    wasps podem ser spawnadas segurando e soltando o botao direito do mouse

--- game/IA_Drone_Proj/Assets/Drone_Player/PlayerScripts/PlayerWaspAttack.cs
+++ game/IA_Drone_Proj/Assets/Drone_Player/PlayerScripts/PlayerWaspAttack.cs
@@ -1,51 +1,66 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 public class PlayerWaspAttack : MonoBehaviour{
     [SerializeField] private GameObject waspsHolder = null;
-    [SerializeField] private float attackTime = 1;
-    [Header("Fuel")]
-    [SerializeField] private float startWaspFuel = 0;
-    [SerializeField] private float maxWaspFuel = 5;
-    private bool canUseWasps = true;
-    private float waspFuel;
+    [SerializeField] private GameObject wasps = null;
+    [SerializeField] private Image waspsTimer = null;
+    [SerializeField] private GameObject waspsAimEffect = null;
+    [SerializeField] private KeyCode cancelAimButton = KeyCode.C;
+    private bool canUseWasps = false;
     private Camera mainCamera;
     private void Awake() {
         mainCamera = FindObjectOfType<Camera>();
-        waspFuel = startWaspFuel;
     }
     public void CheckWapsAttack(){
-        if(Input.GetMouseButtonDown(1)){
+        TougleAimCheck(MouseClick(1));
+        if(canUseWasps){
+            OtherClicksCheck(MouseHold(1), MouseRelease(1));
+        }
+    }
+    private void TougleAimCheck(bool aimToggle){
+        if(aimToggle){
+            TimerEffect(false);
+            SendWasps(false);
+            canUseWasps = !canUseWasps;
+            AimEffect(canUseWasps);
+        }
+    }
+    private void OtherClicksCheck(bool _aiming, bool _releasedAim){
+        if(_aiming){
             SetAttackPosition();
-            CheckAttack();
+            TougleAimCheck(CancelAimButton());
             return;
         }
-    }
-    void CheckAttack(){
-        if(canUseWasps && (waspFuel > 0)){
-            Attack();
-        }
-    }
-    void Attack(){
-        UpdateWaspFuel(-(attackTime/Time.deltaTime));
-        SetWasp(true);            
-    }
-    
-    public void UpdateWaspFuel(float _amt){
-        waspFuel +=_amt;
-        if(waspFuel > maxWaspFuel){
-            waspFuel = maxWaspFuel;
+        if(_releasedAim){
+            AimEffect(false);
+            TryToAttack();
             return;
         }
-        if(waspFuel < 0){
-            waspFuel = 0;
-            return;
-        }
+        return;
     }
-    private void SetWasp(bool b){
-        waspsHolder.SetActive(b);
+    private void TryToAttack(){
+        TimerEffect(true);
+        SendWasps(true);
+        return;
     }
-
+    private void SendWasps(bool b){
+        wasps.SetActive(b);
+    }
+    private bool MouseClick(int i){
+        return Input.GetMouseButtonDown(i);
+    }
+    private bool CancelAimButton(){
+        return Input.GetKey(cancelAimButton);
+    }
+    private bool MouseHold(int i){
+        return Input.GetMouseButton(i);
+    }
+    private bool MouseRelease(int i){
+        return Input.GetMouseButtonUp(i);
+    }
     private void SetAttackPosition(){
         Ray cameraRay = mainCamera.ScreenPointToRay(Input.mousePosition);
         Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
@@ -53,12 +68,32 @@ public class PlayerWaspAttack : MonoBehaviour{
         if(groundPlane.Raycast(cameraRay, out rayLenght)){
             Vector3 pointToLook = (cameraRay.GetPoint(rayLenght));
             Vector3 finalPoint = new Vector3 (pointToLook.x,  0.3f  , pointToLook.z);
-            SetWaspsPosition(finalPoint);
+            WaspsHolderPosition(finalPoint);
             return;
         }
         Debug.LogWarning("impossivel atacar aqui. nao há chao");
     }
-    private void SetWaspsPosition(Vector3 pos){
+    private void WaspsHolderPosition(Vector3 pos){
         waspsHolder.transform.position = new Vector3 (pos.x, 0.3f, pos.z);
+    }
+
+    private void UpdateTimerUI(float amt){
+        waspsTimer.fillAmount = amt;
+    }
+
+    private void TimerEffect(bool active){
+        waspsTimer.gameObject.SetActive(active);
+    }
+
+    private void AimEffect(bool active){
+        waspsAimEffect.SetActive(active);
+    }
+    private void AttackingUI(){
+        AimEffect(false);
+        TimerEffect(true);
+    }
+    private void AimingUI(){
+        TimerEffect(false);
+        AimEffect(true);
     }
 }
